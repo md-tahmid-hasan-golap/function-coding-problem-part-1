56e5c4333b7ecb71d5b7cfd25c9e50b9622fdcac
golap-raj <golapraj47@gmail.com>
2025-02-14 16:56:29 +0600

    add paramitar

---
+++ paramitar.js
@@ -0,0 +1,39 @@
+function add (a, b){
+    const result = a + b;
+    console.log(result)
+}
+add(12, 44);
+add(60, 140);
+
+function sqauare (x, y){
+    const result = x - y;
+    console.log(result)
+}
+sqauare(123, 66)
+sqauare(12, 4);
+
+
+function addAll (a, b, c, d){
+    const allResult = a + b + c + d;
+    console.log(allResult)
+
+}
+addAll(12, 33, 55, 77);
+addAll(10, 20, 30, 40);
+
+
+function gunNumber (a, b, c){
+    const gunfall = a * b * c;
+    console.log(gunfall)
+
+}
+gunNumber(10, 22, 44)
+gunNumber(2, 3, 4)
+
+function double (number){
+    const result = number * 2;
+    console.log(result)
+
+}
+double(10);
+double(100)
