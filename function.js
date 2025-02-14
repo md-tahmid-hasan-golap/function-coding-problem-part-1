69fbb54bebc51e47b8b53cdd19c5355a33693902
golap-raj <golapraj47@gmail.com>
2025-02-14 16:45:49 +0600

    add function

---
+++ function.js
@@ -0,0 +1,11 @@
+// function 
+
+// function diclaration () {}
+
+function tootBrash(){
+    console.log("prothome brash nou");
+    console.log("tar pore past nou");
+    console.log("tar pore ghosa ghoshi koro")
+}
+tootBrash()
+
