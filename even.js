81ab996951774c6266340a6cf3513c3173471dbf
golap-raj <golapraj47@gmail.com>
2025-02-14 18:20:59 +0600

    even

---
+++ even.js
@@ -0,0 +1,17 @@
+function evenNumber (numbers){
+    
+    // console.log(numbers)
+
+    for(let number of numbers){
+        // console.log(number)
+        if(number % 2 === 1){
+            console.log(number)
+            
+        }
+    }
+    return number
+   
+}
+const number = [11, 44, 66, 13, 97, 55]
+const num = evenNumber(number);
+console.log(num);
