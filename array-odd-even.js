c0801647132cd177a65b29c1510ae4cc3b268e4a
golap-raj <golapraj47@gmail.com>
2025-02-14 22:25:27 +0600

    add even number

---
+++ array-odd-even.js
@@ -0,0 +1,91 @@
+// function evenOdd(numbers){
+//     console.log(numbers)
+
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+// function evenOdd(numbers){
+//     for(let number of numbers){
+//         console.log(number)
+//     }
+
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+// function evenOdd(numbers){
+
+//     for(let number of numbers){
+//         if(number % 2 === 1){
+//             console.log(number)
+//         }
+//     }
+
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+// function evenOdd(numbers){
+
+//     for(let number of numbers){
+//         if(number % 2 === 0){
+//             console.log(number)
+//         }
+//     }
+
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+// function evenOdd(numbers){
+//     const array = []
+
+//     for(let number of numbers){
+//         if(number % 2 === 0){
+//             // console.log(number);
+//             array.push(number)
+//         }
+//     }
+// return array
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+// function evenOdd(numbers){
+//     let sum = 0
+
+//     for(let number of numbers){
+//         if(number % 2 === 0){
+//             console.log(number)
+//             sum = sum + number
+            
+//         }
+//     }
+// return sum
+// }
+
+// const number = [12, 33, 55, 66, 77,88];
+// const output = evenOdd(number)
+// console.log(output)
+function evenOdd(numbers){
+    let sum = 0
+
+    for(let number of numbers){
+        if(number % 2 === 1){
+            console.log(number)
+            sum = sum + number
+            
+        }
+    }
+return sum
+}
+
+const number = [12, 33, 55, 66, 77,88];
+const output = evenOdd(number)
+console.log(output)
