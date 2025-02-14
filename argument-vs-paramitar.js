10dcd530ba729c1738fdd676f5c3ffd3be1a7d3d
golap-raj <golapraj47@gmail.com>
2025-02-14 17:20:34 +0600

    add argument

---
+++ argument-vs-paramitar.js
@@ -0,0 +1,13 @@
+// number digun kora ba double kora
+
+function doubleIt(number){
+    const double = number * 2;
+    console.log(number, double)
+
+}
+doubleIt(20);
+doubleIt(30)
+doubleIt(50)
+
+const money = 1000;
+doubleIt(money)
