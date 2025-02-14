cac6fe89eb71682f07e6d29900f38c9829815b75
golap-raj <golapraj47@gmail.com>
2025-02-14 23:49:48 +0600

    add swap

---
+++ date.js
@@ -0,0 +1,5 @@
+const date = new Date() 
+console.log(date)
+
+const newDate = new Date ('2025-10-19');
+console.log(newDate.getMonth())
---
+++ swap.js
@@ -0,0 +1,25 @@
+// let a = 5;
+// let b = 7;
+// console.log(a, b);
+
+// a = b;
+// b = a
+// console.log(a, b)
+
+
+// let a = 5;
+// let b = 7;
+// console.log(a, b);
+// const tamp = a
+// a = b;
+// b = tamp
+// console.log(a, b)
+
+let x = 10;
+let y = 20;
+console.log(x, y)
+
+const swap = x;
+x = y ;
+y = swap
+console.log(x, y)
